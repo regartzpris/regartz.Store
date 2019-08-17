56fa7aece801fa7f2dea352f973c2e3c88bc94f4
Priskila Ritonga <ritongapriskila@gmail.com>
2019-08-17 22:59:00 +0700

    add shoppage,collectionpreview,colletion item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from '../src/pages/shop/shoppage';
 
 import './App.css';
+
+
+
+
+
+
+
+
 
 function App() {
   return (
     <div>
 
       <Route exact path='/' component={HomePage} />
-
+      <Route exact path='/shop' component={ShopPage} />
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
