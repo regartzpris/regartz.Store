5983374bd492009ce86c01aa29d0b3daa1cd23fe
Priskila Ritonga <ritongapriskila@gmail.com>
2019-08-22 03:16:19 +0700

    add cart toggle with reducer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ import '../header/header.styles.scss';
 
 
 
-const Header=({currentUser})=>(
+const Header=({currentUser,hidden})=>(
     <div className='header'>
     <Link className='logo-container' to='/'>
         RegartzStore
@@ -36,15 +36,19 @@ const Header=({currentUser})=>(
       <CartIcon />
 
     </div>
-    <Cart />
+    {
+      hidden ? null :   <Cart />
+    }
+  
 
 </div>
 
 )
    
 
-const mapStateToProps=state=>({
-  currentUser:state.user.currentUser
+const mapStateToProps=({user:{currentUser},cart:{hidden}})=>({
+  currentUser,
+  hidden
 })
 
 export default connect (mapStateToProps,null)(Header);
---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,25 @@
+import CartActionTypes from './cart.types';
+
+
+const initial_state={
+
+    hidden:true
+
+}
+
+
+const cartReducer=(state=initial_state,action)=>{
+    switch(action.type){
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
+            return{
+                ...state,
+                hidden:!state.hidden
+            }
+
+            default:
+                return state;
+    }
+};
+
+
+export default cartReducer;
