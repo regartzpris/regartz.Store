d9a33467cfeeb874f22d0fe9c0c46a14abb875a6
Priskila Ritonga <ritongapriskila@gmail.com>
2019-08-26 05:13:11 +0700

    add cartitem component and connect to redux

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button';
+import CartItem from '../cart-item/cart-item';
 
 import '../cart/cart.styles.scss';
 
 
-const Cart=()=>(
+const Cart = ({cartItems}) => (
     <div className='cart'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem=>(
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            }
+
+
+
+        </div>
         <CustomButton>CHECK OUT</CustomButton>
 
-        
+
     </div>
-)
+);
 
 
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
